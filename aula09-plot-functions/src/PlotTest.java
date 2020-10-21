4d1f1ae436b9a3219d1a38c45ed8e1a9518f53fb
Prof. Diogo S. Martins <santana.martins@ufabc.edu.br>
2020-10-20 21:58:39 -0300

    polymorphism

---
+++ aula09-plot-functions/src/PlotTest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class PlotTest {
+    public static void main(String[] args) {
+        // y = x
+        double[] y = new double[30];
+
+        for (int i = 0; i < 30; i++) {
+            y[i] = (double )i / 10;
+        }
+
+        System.out.println(Arrays.toString(y));
+
+        StdStats.plotPoints(y);
+        StdStats.plotLines(y);
+    }
+}
